a0af740d215662e70648169db217932442c38fbd
Victor Gonzalez <dazedbutamused@gmail.com>
2025-06-09 23:24:17 -0700

    buncha shit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import { generateText } from 'ai';
+import { createOpenAI } from '@ai-sdk/openai';
+import { useState, useRef } from 'react';
+import { QueryClient, QueryClientProvider, useMutation, useQuery } from '@tanstack/react-query';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { v4 } from 'uuid';
+const queryClient = new QueryClient();
+
+const openai = createOpenAI();
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  console.log('hits');
+  const ref = useRef('');
+  interface ResponseItem {
+    text: string;
+    uuid: string;
+  }
+  const [responseList, setResponseList] = useState<ResponseItem[]>([]);
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ['ask'],
+    enabled: false,
+    queryFn: async () => {
+      if (!prompt) return;
+      const { text } = await generateText({
+        model: openai('gpt-4o-mini'),
+        prompt: ref.current,
+      });
+      setResponseList([...responseList, { text, uuid: v4() }]);
+      return prompt;
+    },
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const submitPrompt = () => refetch();
+  const handleKeyDown = (key: string) => {
+    if (key !== 'Enter') return;
+    submitPrompt();
+  };
+
+  const ResponseItem = ({ text, uuid }: ResponseItem) => (
+    <li id={uuid} key={uuid}>
+      {text}
+    </li>
+  );
+  const ResponseList = () => {
+    console.count('rerender');
+    return <ul>{responseList.map(ResponseItem)}</ul>;
+  };
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+        <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+          <Card className='w-[50vw]'>
+            <div>Ask a question</div>
+            <CardContent>
+              <Input
+                type='text'
+                onKeyDown={(e) => handleKeyDown(e.key)}
+                onChange={(e) => (ref.current = e.target.value)}
+              ></Input>
+              <Button onClick={submitPrompt}>Submit</Button>
+              <ResponseList />
+            </CardContent>
+          </Card>
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
