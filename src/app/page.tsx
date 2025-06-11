ba29f75c7c2e3b6464c0ad972952498bf3afc97b
Victor Gonzalez <dazedbutamused@gmail.com>
2025-06-10 18:33:11 -0700

    somethin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,70 +2,91 @@
 import Image from 'next/image';
 import { generateText } from 'ai';
 import { createOpenAI } from '@ai-sdk/openai';
-import { useState, useRef } from 'react';
 import { QueryClient, QueryClientProvider, useMutation, useQuery } from '@tanstack/react-query';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { v4 } from 'uuid';
+import { create } from 'zustand';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useRef } from 'react';
 const queryClient = new QueryClient();
 
-const openai = createOpenAI();
+const openai = createOpenAI({
+  apiKey: ''
+});
 
-export default function Home() {
-  console.log('hits');
-  const ref = useRef('');
-  interface ResponseItem {
-    text: string;
-    uuid: string;
-  }
-  const [responseList, setResponseList] = useState<ResponseItem[]>([]);
-  const { data, isLoading, isError, refetch } = useQuery({
+interface ResponseItem {
+  isPending?: boolean;
+  text?: string;
+  uuid: string;
+}
+
+interface ResponseStoreState {
+  list: ResponseItem[];
+  update: (responseList: ResponseItem[]) => void;
+}
+const useResponseStore = create<ResponseStoreState>((set) => ({
+  list: [],
+  update: (updatedList: ResponseItem[]) => set({ list: [...updatedList] }),
+}));
+
+const ResponseItem = ({ isPending, text, uuid }: ResponseItem) => (
+  <li id={uuid} key={uuid}>
+    {isPending ? 'Loading...' : text}
+  </li>
+);
+const ResponseList = ({ list }: { list: ResponseItem[] }) => <ul>{list.map(ResponseItem)}</ul>;
+const PromptCard = () => {
+  const prompt = useRef('');
+  const store = useResponseStore();
+  const { refetch } = useQuery({
     queryKey: ['ask'],
     enabled: false,
     queryFn: async () => {
-      if (!prompt) return;
       const { text } = await generateText({
         model: openai('gpt-4o-mini'),
-        prompt: ref.current,
+        prompt: prompt.current,
       });
-      setResponseList([...responseList, { text, uuid: v4() }]);
-      return prompt;
+      store.update([...store.list.slice(0, -1), { text, uuid: v4() }]);
     },
   });
-
-  const submitPrompt = () => refetch();
-  const handleKeyDown = (key: string) => {
-    if (key !== 'Enter') return;
-    submitPrompt();
+  const PromptForm: React.FC = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit: SubmitHandler<any> = (data) => {
+      if (!data.prompt) return;
+      prompt.current = data.prompt;
+      store.list.push({ isPending: true, uuid: v4() })
+      store.update(store.list);
+      refetch();
+    };
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input className='my-1' type='text' {...register('prompt')}></Input>
+        <Button className='my-1' type='submit'>
+          Submit
+        </Button>
+      </form>
+    );
   };
 
-  const ResponseItem = ({ text, uuid }: ResponseItem) => (
-    <li id={uuid} key={uuid}>
-      {text}
-    </li>
+  return (
+    <Card className='w-[50vw]'>
+      <CardContent>
+        <h2>Ask a question</h2>
+        <PromptForm />
+        <ResponseList list={store.list} />
+      </CardContent>
+    </Card>
   );
-  const ResponseList = () => {
-    console.count('rerender');
-    return <ul>{responseList.map(ResponseItem)}</ul>;
-  };
+};
 
+export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
         <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-          <Card className='w-[50vw]'>
-            <div>Ask a question</div>
-            <CardContent>
-              <Input
-                type='text'
-                onKeyDown={(e) => handleKeyDown(e.key)}
-                onChange={(e) => (ref.current = e.target.value)}
-              ></Input>
-              <Button onClick={submitPrompt}>Submit</Button>
-              <ResponseList />
-            </CardContent>
-          </Card>
+          <PromptCard />
         </main>
       </div>
     </QueryClientProvider>
